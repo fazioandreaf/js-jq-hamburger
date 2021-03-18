683fece5e10848ee4ee5f2b46d0a5a564e4abdfd
fazioandrea.f <fazioandrea.f@gmail.com>
2021-03-18 15:57:47 +0100

    apertura e chiusura hamburger menu

--- main.js
+++ main.js
@@ -4,5 +4,12 @@ function openHM() {
         $('.hamburger-menu').show();
     }
     )
+}function closeHM() {
+    $('.fa-times').click(function(){
+        $('.hamburger-menu').hide(); 
+    }
+    )
 }
+
 openHM()
+closeHM()
