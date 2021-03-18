44d327226d1f07ba96bd5836e90a09883b26df63
fazioandrea.f <fazioandrea.f@gmail.com>
2021-03-18 15:32:37 +0100

    .

---
+++ main.js
@@ -0,0 +1,8 @@
+function openHM() {
+    $('.fa-bars').click(function(){
+        // $('.hamburger-menu').css('display','block'); Stesso risultato
+        $('.hamburger-menu').show();
+    }
+    )
+}
+openHM()
